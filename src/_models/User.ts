93f1ec68aa0190fa4126370d55d8d74f38ba156f
Elise-Pelletier <elletier.elise36@gmail.com>
2023-01-02 11:53:34 +0100

    modification des modèles event et user et création du modèle formation

--- src/_models/User.ts
+++ src/_models/User.ts
@@ -2,8 +2,11 @@ interface User {
   firstname: string
   lastname: string
   username: string
+  image: string
   birthday: Date
   email: string
+  formation: Formation
+  school_location: string
   password: string
   roles: [string]
 }
