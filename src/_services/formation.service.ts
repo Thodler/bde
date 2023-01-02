debfa72d3722f8f0b1718dbeff53427c28399b54
Thodler <neoskywalker01@gmail.com>
2023-01-02 21:00:46 +0100

    [UPD] Ajout des methods get dans Formations.ts

--- src/_services/formation.service.ts
+++ src/_services/formation.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
 
-export const formations: Formation[] = [
-  {'id': 1,'name': 'Informatique', 'school': 'Ajaccio'},
-  {'id': 2,'name': 'Gestion des entreprises', 'school': 'Bastia'},
-  {'id': 3,'name': 'Sciences politiques', 'school': 'Ajaccio'},
-  {'id': 4,'name': 'Droit', 'school': 'Bastia'},
-  {'id': 5,'name': 'Littérature', 'school': 'Ajaccio'},
-  {'id': 6,'name': 'Sciences de la santé', 'school': 'Bastia'}
-]
-
 @Injectable({
   providedIn: 'root'
 })
 export class FormationService {
 
+  formations: Formation[] = [
+    {'id': 1,'name': 'Informatique', 'school': 'Ajaccio'},
+    {'id': 2,'name': 'Gestion des entreprises', 'school': 'Bastia'},
+    {'id': 3,'name': 'Sciences politiques', 'school': 'Ajaccio'},
+    {'id': 4,'name': 'Droit', 'school': 'Bastia'},
+    {'id': 5,'name': 'Littérature', 'school': 'Ajaccio'},
+    {'id': 6,'name': 'Sciences de la santé', 'school': 'Bastia'}
+  ]
+
   constructor() { }
+
+  getFormations(): Formation[]
+  {
+    return this.formations
+  }
+
+  getFormationById(id: number): Formation | null
+  {
+    for (const formation of this.formations) {
+      if(formation.id === id )return formation
+    }
+    return null
+  }
+
 }
