ae5f41b91438065c429eb641657f617a1ce6fd67
Thodler <neoskywalker01@gmail.com>
2023-01-02 22:53:49 +0100

    [UPD] Import des Components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {HeaderComponent} from "./helpers/header/header.component";
+import {UsersListComponent} from "./pages/users-list/users-list.component";
+import {LoginComponent} from "./pages/login/login.component";
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UsersListComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
