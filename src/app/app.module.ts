bbeb73e58f96f337e26ce0d7d1665a0e81e925a2
Thodler <neoskywalker01@gmail.com>
2023-01-03 09:23:26 +0100

    [FIX] Ajout de component dans app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {HeaderComponent} from "./helpers/header/header.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {UsersListComponent} from "./pages/users-list/users-list.component";
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent],
+    declarations: [
+      AppComponent,
+      HeaderComponent,
+      LoginComponent,
+      UsersListComponent
+    ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
