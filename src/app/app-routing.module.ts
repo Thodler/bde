f7e39f88a3dc7162c2caec3badd2ce8cb72f7fa0
Thodler <neoskywalker01@gmail.com>
2023-01-03 16:02:35 +0100

    [ADD] Component NewsFeed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {UsersListComponent} from "./pages/users-list/users-list.component";
+import {NewsFeedComponent} from "./pages/news-feed/news-feed.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'users', component: UsersListComponent}
+  { path: 'users', component: UsersListComponent},
+  { path: 'newsfeed', component: NewsFeedComponent},
+
+
 ];
 
 @NgModule({
