20a73c2147b6280b05f18c46319774c448b9070d
Thodler <neoskywalker01@gmail.com>
2023-01-04 10:02:16 +0100

    [UPD] Mise en place d'un Toast pour la boite à idée.

--- src/app/pages/idea-add/idea-add.component.ts
+++ src/app/pages/idea-add/idea-add.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Idea} from '../../../_models/Idea';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IdeaService } from 'src/_services/idea.service';
+import {ToastController} from "@ionic/angular";
 
 @Component({
   selector: 'app-idea-add',
@@ -16,8 +17,9 @@ export class IdeaAddComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private ideaService: IdeaService,
+    private toastController: ToastController
     ) {
-    
+
    }
 
   ngOnInit() {
@@ -32,6 +34,17 @@ export class IdeaAddComponent implements OnInit {
 
   onSubmit() {
     this.ideaService.addIdea(this.formIdea.value);
+  }
 
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    this.onSubmit();
+    this.formIdea = this.initForm();
+    const toast = await this.toastController.create({
+      message: 'Votre idée a bien été ajouter!',
+      duration: 1500,
+      position: position
+    });
+
+    await toast.present();
   }
 }
