215d2a2b3793df0aec605c0006495d1322dec2ff
Thodler <neoskywalker01@gmail.com>
2023-01-05 21:46:15 +0100

    [UPD] Inscriuption ou desinscription de l'utilisateur courant à un evenement.

--- src/app/forms/user-form/user-form.component.ts
+++ src/app/forms/user-form/user-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {PhotoService} from "../../../_services/photo.service";
+import {User} from "../../../_models/User";
+import {UserService} from "../../../_services/user.service";
 
 @Component({
   selector: 'app-user-form',
@@ -13,12 +15,32 @@ export class UserFormComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private photoService: PhotoService
+    private photoService: PhotoService,
+    private userService: UserService
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initForm(this.userService.getUserStorage());
+  }
+
+  initForm(user: User){
+    this.formUser = this.formBuilder.group({
+      firstname: user.firstname,
+      lastname: user.lastname,
+      username: user.username,
+      email: user.email,
+      birthday: user.birthday,
+      formation: user.formation,
+      discord: user.discord,
+      password: user.password
+    })
+  }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+
+  onSubmit() {
+    console.log(this.formUser.value)
   }
 }
--- src/app/pages/event-list/event-list.component.ts
+++ src/app/pages/event-list/event-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, } from '@angular/core';
 import { EventService } from 'src/_services/event.service';
 import { Event } from '../../../_models/Event';
+import {User} from "../../../_models/User";
+import {UserService} from "../../../_services/user.service";
+import {exit} from "ionicons/icons";
 
 
 @Component({
@@ -11,18 +14,36 @@ import { Event } from '../../../_models/Event';
 export class EventListComponent implements OnInit {
 
   events!: Event[]
+  userCurrent!: User
+  colorButton!:string
+  textButton!:string
 
   constructor(
     private eventService: EventService,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
-    this.events = this.eventService.getEvents(); 
-    
+    this.events = this.eventService.getEvents();
+    this.userCurrent = this.userService.getUserStorage();
+    this.colorButton = 'primary'
+    this.textButton = 'je participe'
   }
 
-  onSubscribeEvent()
-  {}
-
+  checkInscription(inscrits: User[]){
+    return inscrits.includes(this.userCurrent)
+  }
+  onSubscribeEvent(inscrits: User[]):void
+  {
+    if(!this.checkInscription(inscrits)){
+      inscrits.push(this.userCurrent)
+    }else{
+      inscrits.forEach((user:User , index:number )=> {
+        if(user.id === this.userCurrent.id){
+          inscrits.splice(index, 1)
+        }
+      })
+    }
+  }
 
 }
